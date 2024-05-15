7fd01a17905d00d314a2a97ab84bf32af430972c
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2024-05-15 20:33:03 +0900

    [유형별] DFS,BFS/특정 거리의 도시 찾기 (정답 참고)

---
+++ 유형별/3. DFS&BFS/특정 거리의 도시 찾기.py
@@ -0,0 +1,47 @@
+"""
+4 4 2 1
+1 2
+1 3
+2 3
+2 4
+
+4 3 2 1
+1 2
+1 3
+1 4
+
+4 4 1 1
+1 2
+1 3
+2 3
+2 4
+"""
+
+from collections import deque
+
+n, m, k, x = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+
+distance = [-1] * (n+1)
+distance[x] = 0
+
+q = deque([x])
+while q:
+    now = q.popleft()
+    for next_node in graph[now]:
+        if distance[next_node] == -1:
+            distance[next_node] = distance[now] + 1
+            q.append(next_node)
+
+check = False
+for i in range(1, n + 1):
+    if distance[i] == k:
+        print(i)
+        check = True
+
+if check == False:
+    print(-1)
