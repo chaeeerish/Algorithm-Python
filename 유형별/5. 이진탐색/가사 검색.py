fe17ff0717a370af6158aa9354d02ab8be9dfef2
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2024-05-21 04:04:17 +0900

    [유형별] 이진 탐색/가사 검색 (효율성 통과)

--- 유형별/5. 이진탐색/가사 검색.py
+++ 유형별/5. 이진탐색/가사 검색.py
@@ -1,12 +1,31 @@
+from collections import defaultdict
+from bisect import bisect_left, bisect_right
+
+def count_by_lange(lst, start, end):
+    return bisect_right(lst, end) - bisect_left(lst, start)
+
 def solution(words, queries):
     answer = []
+    cands = defaultdict(list)
+    reverse_cands = defaultdict(list)
+
+    for word in words:
+        cands[len(word)].append(word)
+        reverse_cands[len(word)].append(word[::-1])
+    for cand in cands.values():
+        cand.sort()
+    for cand in reverse_cands.values():
+        cand.sort()
 
     for query in queries:
-        start, end = query.find('?'), query.rfind('?')
-        keyword = query[:start] + query[end+1:]
-        filtered = [word[:start] + word[end+1:] for word in words if len(word) == len(query) and word[:start] + word[end+1:] == keyword]
-        answer.append(len(filtered))
+        if query[0] == '?':
+            lst = reverse_cands[len(query)]
+            start, end = query[::-1].replace('?', 'a'), query[::-1].replace('?', 'z')
+        else:
+            lst = cands[len(query)]
+            start, end = query.replace('?', 'a'), query.replace('?', 'z')
+        answer.append(count_by_lange(lst, start, end))
+
     return answer
 
-# [3, 2, 4, 1, 0]
 print(solution(["frodo", "front", "frost", "frozen", "frame", "kakao"], ["fro??", "????o", "fr???", "fro???", "pro?"]))
