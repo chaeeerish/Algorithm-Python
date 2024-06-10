9b4139607e05b9f4591a81dca1825e85a8298472
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2024-06-10 23:48:57 +0900

    [교재별] 재귀/호텔 방 배정 (효율성 테스트 통과, 해설 참고)

--- 교재별/프로그래머스 코딩 테스트 문제 풀이 전략/5장. 재귀/호텔 방 배정.py
+++ 교재별/프로그래머스 코딩 테스트 문제 풀이 전략/5장. 재귀/호텔 방 배정.py
@@ -1,22 +1,23 @@
+import sys
+sys.setrecursionlimit(100000)
+
+
+def find(number, room):
+    if number not in room:
+        room[number] = number + 1
+        return number
+
+    empty = find(room[number], room)
+    room[number] = empty + 1
+    return empty
+
+
 def solution(k, room_number):
     answer = []
-    is_allocated = {}
-    for i in range(1, k + 1):
-        is_allocated[i] = False
+    dic = {}
 
-    for wish_number in room_number:
-        if is_allocated[wish_number] == False:
-            is_allocated[wish_number] = True
-            answer.append(wish_number)
-        else:
-            while True:
-                wish_number += 1
-                if is_allocated[wish_number] == False:
-                    is_allocated[wish_number] = True
-                    answer.append(wish_number)
-                    break
+    for i in room_number:
+        num = find(i, dic)
+        answer.append(num)
 
     return answer
-
-# [1, 3, 4, 2, 5, 6]
-print(solution(10, [1, 3, 4, 1, 3, 1]))
