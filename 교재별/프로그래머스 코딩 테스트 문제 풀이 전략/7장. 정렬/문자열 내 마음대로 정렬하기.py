40647218190627003cd9a38bf5c61737b9e532c4
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2024-06-11 15:59:12 +0900

    [교재별] 정렬/문자열 내 마음대로 정렬하기 (코드 압축)

--- 교재별/프로그래머스 코딩 테스트 문제 풀이 전략/7장. 정렬/문자열 내 마음대로 정렬하기.py
+++ 교재별/프로그래머스 코딩 테스트 문제 풀이 전략/7장. 정렬/문자열 내 마음대로 정렬하기.py
@@ -1,24 +1,5 @@
 def solution(strings, n):
-    strings.sort(key=lambda x: x[n])
-
-    answer = []
-
-    sort_list = []
-    sort_list.append(strings[0])
-    cnt = 1
-    while cnt <= len(strings) - 1:
-        if strings[cnt - 1][n] == strings[cnt][n]:
-            sort_list.append(strings[cnt])
-        else:
-            sort_list.sort()
-            answer.extend(sort_list)
-            sort_list.clear()
-            sort_list.append(strings[cnt])
-        cnt += 1
-
-    sort_list.sort()
-    answer.extend(sort_list)
-    return answer
+    return sorted(strings, key=lambda x: (x[n], x))
 
 # ["car", "bed", "sun"]
 print(solution(["sun", "bed", "car"], 1))
