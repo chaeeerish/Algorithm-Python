df5d89dbc8f17942968d5b6e2db8cd72e5ec4f5f
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2024-06-13 01:58:57 +0900

    [교재별] 이진탐색/입국심사 (해설 참고, 제출 통과)

--- 교재별/프로그래머스 코딩 테스트 문제 풀이 전략/8장. 이진 탐색/입국심사.py
+++ 교재별/프로그래머스 코딩 테스트 문제 풀이 전략/8장. 이진 탐색/입국심사.py
@@ -1,26 +1,23 @@
-import math
-
-
-def 시간_계산(입국_심사대_리스트, 받은_사람들):
-    return max(받은_사람들[i] * 입국_심사대_리스트[i] for i in range(len(입국_심사대_리스트)))
-
-
-def 입국_심사(n, h, times, 입국_심사대_리스트):
-    if n == 0 and h == 0:
-        return 시간_계산(times, 입국_심사대_리스트)
-    elif n != 0 and h == 0:
-        return math.inf
-    elif n == 0 and h != 0:
-        return 시간_계산(times, 입국_심사대_리스트 + ([0] * h))
-
-    최소_시간 = math.inf
-    for i in range(0, n + 1):
-        최소_시간 = min(최소_시간, 입국_심사(n-i, h-1, times, 입국_심사대_리스트 + [i]))
-    return 최소_시간
-
-
 def solution(n, times):
-    return 입국_심사(n, len(times), times, [])
+    left = 1
+    right = max(times) * n
+
+    while left <= right:
+        mid = (left + right) // 2
+        people = 0
+
+        for time in times:
+            people += mid // time
+
+            if people >= n:
+                break
+
+        if people >= n:
+            answer = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+    return answer
 
 # 28
 print(solution(6, [7, 10]))
