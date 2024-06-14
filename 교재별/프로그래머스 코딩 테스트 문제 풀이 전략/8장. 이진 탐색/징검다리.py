45d110156a341f847a17527d9b5cd55249dab17d
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2024-06-14 15:39:08 +0900

    [교재별] 이진탐색/징검다리 (해설 참고, 제출 통과)

--- 교재별/프로그래머스 코딩 테스트 문제 풀이 전략/8장. 이진 탐색/징검다리.py
+++ 교재별/프로그래머스 코딩 테스트 문제 풀이 전략/8장. 이진 탐색/징검다리.py
@@ -1,38 +1,38 @@
-import itertools
-
-def get_new_바위_사이_리스트(바위_사이_리스트, 제외할_바위):
-    new_바위_사이_리스트 = []
-
-    i = 0
-    while i < len(바위_사이_리스트):
-        if i not in 제외할_바위:
-            new_바위_사이_리스트.append(바위_사이_리스트[i])
-        else:
-            new_바위_사이_리스트.append(바위_사이_리스트[i] + 바위_사이_리스트[i + 1])
-            i += 1
-        i += 1
-
-    return new_바위_사이_리스트
-
 def solution(distance, rocks, n):
+    answer = -1
+
     rocks.sort()
 
-    바위_사이_리스트 = []
-    바위_사이_리스트.append(rocks[0])
-    for i in range(len(rocks) - 1):
-        바위_사이_리스트.append(rocks[i + 1] - rocks[i])
-    바위_사이_리스트.append(distance - rocks[-1])
+    left = 1
+    right = distance
+    rocks.append(distance)
 
-    최소_거리 = -1
-    # rock의 길이(5) 중에 2개 고르기
-    for iter in itertools.combinations([i for i in range(len(rocks))], n):
-        new_바위_사이_리스트 = get_new_바위_사이_리스트(바위_사이_리스트, iter)
-        최소_거리 = max(최소_거리, min(new_바위_사이_리스트))
+    while left <= right:
+        mid = (left + right) // 2
 
-    return 최소_거리
+        delete = 0
+        pre_rock = 0 # 이전 바위의 위치
+        for rock in rocks:
+            dist = rock - pre_rock
+            if dist < mid:
+                delete += 1
+
+                if delete > n:
+                    break
+            else:
+                pre_rock = rock
+
+        if delete > n:
+            right = mid - 1
+        else:
+            answer = max(answer, mid)
+            left = mid + 1
+
+    return answer
 
 # 0 2 11 14 17 21 25
 #  2 9  3  3  4  4
+# (sort 후) 2 3 3 4 4 9
 # 2개 제거 하면 -> 바위 사이의 거리 리스트에서도 2개가 제거된다
 # 2 9 3 11 - [17, 21] 제거
 # 11 3 3 8 - [2, 21] 제거
