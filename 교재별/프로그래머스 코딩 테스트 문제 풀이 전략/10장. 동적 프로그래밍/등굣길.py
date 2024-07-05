81a19ac10124a7aace59a97668cba44a400b41a2
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2024-07-06 01:06:33 +0900

    [교재별] 동적 프로그래밍/등굣길 (제출 통과, 해설 참고)

--- 교재별/프로그래머스 코딩 테스트 문제 풀이 전략/10장. 동적 프로그래밍/등굣길.py
+++ 교재별/프로그래머스 코딩 테스트 문제 풀이 전략/10장. 동적 프로그래밍/등굣길.py
@@ -1,22 +1,3 @@
-import math
-
-
-def bfs(x, y, m, n, puddles, cur_path, min_path):
-    if [y, x] in puddles:
-        return 0
-
-    if (y == n - 1 and x == m) or (y == n and x == m - 1):
-        if cur_path == min_path:
-            return 1
-        else:
-            return 0
-
-    if y > n or x > m:
-        return 0
-
-    return bfs(x, y + 1, m, n, puddles, cur_path + 1, min_path) + bfs(x + 1, y, m, n, puddles, cur_path + 1, min_path)
-
-
 def solution(m, n, puddles):
     path = [[0] * (m + 1) for _ in range(n + 1)]
 
@@ -26,22 +7,16 @@ def solution(m, n, puddles):
 
     for i in range(1, n + 1):
         for j in range(1, m + 1):
+            if [i, j] in puddles:
+                continue
 
             if i == 1 and j == 1:
-                continue
-            if [i, j] in puddles:
-                path[i][j] = math.inf
-                continue
+                path[i][j] = 1
+            elif i == 1:
+                path[i][j] = path[i][j - 1]
+            elif j == 1:
+                path[i][j] = path[i - 1][j]
+            else:
+                path[i][j] = path[i - 1][j] + path[i][j - 1]
 
-            if i == 1:
-                path[i][j] = path[i][j - 1] + 1
-                continue
-            if j == 1:
-                path[i][j] = path[i - 1][j] + 1
-                continue
-
-            path[i][j] = min(path[i - 1][j], path[i][j - 1]) + 1
-
-    min_path = min(path[n - 1][m], path[n][m - 1])
-    cnt = bfs(1, 1, m, n, puddles, 0, min_path)
-    return cnt % 1000000007
+    return path[n][m] % 1000000007
